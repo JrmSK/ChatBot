fe6a4b196132eb76b4768b3302cf6884d461f4ec
JrmSK <jeremy.skornik@gmail.com>
2018-12-14 23:37:46 +0200

    Profanity check implemented with Profanity api. fixed weather text

--- boto.py
+++ boto.py
@@ -3,12 +3,14 @@ This is the template server side for ChatBot
 """
 from bottle import route, run, template, static_file, request
 from weather import Weather, Unit
+from profanity import profanity
 import json
+
 
 answer_data = {
     'greeting': ["my name"],
-    'counter': 0,  # might delete that
-    'user_answers': []
+    'user_answers': [],
+    'user_name': ''
 }
 
 
@@ -47,41 +49,64 @@ def images(filename):
 # This function checks what kind of message the user input:
 def check_answer(user_message):
     answer_data['user_answers'].append(user_message)
-    split_answer = user_message.split()
+    split_answer = user_message.lower().split()
+
+    if profanity.contains_profanity(user_message):
+        return bad_manners_api()
 
     if len(answer_data['user_answers']) == 1:
         return greet(split_answer[-1])
 
-    for x in answer_data['greeting']:  # In case user wants to change his name... not very useful
-        if x in user_message:
-            return greet(split_answer[-1])
+    if any(x in user_message.lower() for x in answer_data['greeting']):  # In case user wants to change his name... not very useful
+        return greet(split_answer[-1])
 
-    for x in split_answer:  # In case user wants to change his name... not very useful
+    for x in split_answer:
         if x == "help":
             return help()
         elif x == "weather":
             return weather_api()
+        elif x == "movie":
+            return movie()
+        else:
+            return bot_answer('waiting', user_message)
         # OTHERS CHECKS HERE
+
+
+def bot_answer(animation, msg):
+    return json.dumps({"animation": animation, "msg": msg})
 
 
 def greet(str1):
     chatbot_answer = "{0} is a cool name! What can I do for you today? Ask for help if you are lost!".format(str1)
-    return json.dumps({"animation": "inlove", "msg": chatbot_answer})
+    answer_data['user_name'] = str1
+    return bot_answer("in love", chatbot_answer)
 
 
 def help():
     chatbot_answer = "I can: predict the weather, get quotes, tell a joke, talk about a movie, etc..."  # UPDATE HERE with new APIs & co
-    return json.dumps({"animation": "waiting", "msg": chatbot_answer})
+    return bot_answer("waiting", chatbot_answer)
 
 
 # Returns the weather thanks to weather API
 def weather_api():
     weather = Weather(unit=Unit.CELSIUS)
-
     location = weather.lookup_by_location('tel-aviv')
     forecasts = location.forecast
-    chatbot_answer = "Tomorrow in Tel Aviv, the weather will be {0}, with a high at {1} and a low at {2}. The day after, the weather will be {3}.".format(
+    chatbot_answer = "Tomorrow in Tel Aviv, the weather will be {0}, with a high at {1}°C and a low at {2}°C. The day after, the weather will be {3}.".format(
         forecasts[0].text.lower(), forecasts[0].high, forecasts[0].low, forecasts[1].text.lower())
+    return bot_answer("animation", chatbot_answer)
+
+
+
+def bad_manners_api():
+    # CODE HERE
+    chatbot_answer = "Bad language is not allowed in this chat!"
+    return json.dumps({"animation": "crying", "msg": chatbot_answer})
+
+
+def movie():
+    # CODE HERE
+    chatbot_answer = ""
     return json.dumps({"animation": "excited", "msg": chatbot_answer})
 
 
